6cbaa52a1de69a3da4f7b4ef229363fe1e112f2c
unknown <florian.kohler@telecom-bretagne.eu>
2018-02-06 11:39:53 +0100

    new riders and table 2017 with excel analysis

--- script.py
+++ script.py
@@ -1,244 +1,28 @@
-
-# -*- coding: utf-8 -*-
-import sys
+import pandas
+import numpy
 import os
+import collections
+import sys
 import codecs
 import jinja2
-import collections
 import copy
 
-the_dir=os.path.dirname(os.path.abspath(__file__))
 
-fsloader = jinja2.FileSystemLoader(the_dir) #dossier ou se trouve les template
+dir_path=os.path.dirname(os.path.abspath(__file__))
+filename = dir_path + "\\Resultats.xlsx"
+
+content_xlsx = pandas.ExcelFile(filename)
+
+fsloader = jinja2.FileSystemLoader(dir_path) #dossier ou se trouve les template
 env = jinja2.Environment(loader=fsloader)
 
-Places=[]
-#ajout sous la forme [place, 'FSGT' ou 'FFC', '2-3' ou'S4' ou V4' ou 'Min' ou 'Cad' ou bien '123J','23J','3J', 'PassD1','PassD3','Min','Cad' , ' commentaire (nom et course ou date']
-Places.append([10,'FSGT','23','Mathieu Nachbaur 26-03'])
-Places.append([8,'FSGT','23','Mathieu Nachbaur 02-04'])
-Places.append([6,'FSGT','23','Mathieu Nachbaur 08-05'])
-Places.append([9,'FSGT','23','Mathieu Nachbaur 14-05'])
-Places.append([5,'FSGT','23','Mathieu Nachbaur 27-05'])
-Places.append([2,'FSGT','23','Mathieu Nachbaur 25-06'])
-Places.append([5,'FSGT','S4','santo'])
-Places.append([2,'FSGT','S4','Santo'])
-Places.append([1,'FSGT','S4','Santo'])
-Places.append([1,'FSGT','S4','Santo'])
-Places.append([1,'FSGT','23','Julien G Montreux'])
-Places.append([2,'FSGT','23','Julien G Frotey'])
 
-Places.append([10,'FSGT','23','Julien B Frotey'])
-Places.append([4,'FSGT','23','Julien B 04-06'])
-Places.append([2,'FSGT','S4','Cedric L'])
-Places.append([2,'FSGT','S4','Cedric L'])
-Places.append([1,'FSGT','S4','Cedric L'])
-Places.append([2,'FSGT','23','Eric H 02-04'])
-Places.append([3,'FSGT','23','sonke '])
-
-Places.append([4,'FSGT','S4','Antoine'])
-Places.append([6,'FSGT','S4','Antoine'])
-Places.append([3,'FSGT','S4','Antoine'])
-Places.append([6,'FSGT','S4','Antoine'])
-
-Places.append([9,'FSGT','S4','Lucien'])
-Places.append([2,'FSGT','S4','Lucien'])
-Places.append([4,'FSGT','S4','Lucien'])
-Places.append([2,'FSGT','S4','Lucien'])
-
-Places.append([1,'FSGT','23','Jerome H Seppois'])
-
-Places.append([10,'FSGT','S4','Brieuc'])
-Places.append([10,'FSGT','S4','Brieuc'])
-
-Places.append([7,'FSGT','23','Jose L'])
-
-Places.append([5,'FSGT','V4','Jerome J'])
-Places.append([8,'FSGT','V4','Jerome J'])
-Places.append([2,'FSGT','V4','Jerome J'])
-Places.append([4,'FSGT','V4','Jerome J'])
-
-Places.append([8,'FSGT','V4','Claude S'])
-Places.append([3,'FSGT','V4','Claude S'])
-Places.append([2,'FSGT','V4','Claude S'])
-Places.append([9,'FSGT','V4','Claude S'])
-Places.append([6,'FSGT','V4','Claude S'])
-
-Places.append([7,'FSGT','V4','Stephane M'])
-Places.append([7,'FSGT','V4','Stephane M'])
-Places.append([7,'FSGT','V4','Stephane M'])
-
-Places.append([6,'FSGT','V4','Sebastien S'])
-Places.append([4,'FSGT','V4','Sebastien S'])
-Places.append([4,'FSGT','V4','Sebastien S'])
-
-Places.append([5,'FSGT','V4','Lionel B'])
-
-Places.append([8,'FSGT','V4','Rene S'])
-Places.append([9,'FSGT','V4','Rene  S'])
-Places.append([6,'FSGT','V4','Rene  S'])
-Places.append([1,'FSGT','V4','Rene  S'])
-Places.append([1,'FSGT','V4','Rene  S'])
-Places.append([2,'FSGT','V4','Rene  S'])
-Places.append([7,'FSGT','V4','Rene  S'])
-
-Places.append([4,'FSGT','Cad','Johann V'])
-Places.append([8,'FSGT','Cad','Johann V'])
-Places.append([6,'FSGT','Cad','Johann V'])
-Places.append([5,'FSGT','Cad','Johann V'])
-Places.append([9,'FSGT','Cad','Johann V'])
-Places.append([5,'FSGT','Cad','Johann V'])
-
-Places.append([1,'FSGT','Cad','Anthony W'])
-Places.append([2,'FSGT','Cad','Anthony W'])
-Places.append([6,'FSGT','Cad','Anthony W'])
-Places.append([2,'FSGT','Cad','Anthony W'])
-
-Places.append([9,'FSGT','Cad','Clement B'])
-Places.append([5,'FSGT','Cad','Clement B'])
-Places.append([6,'FSGT','Cad','Clement B'])
-
-Places.append([1,'FSGT','Min','Alexandre N'])
-Places.append([1,'FSGT','Min','Alexandre N'])
-Places.append([1,'FSGT','Min','Alexandre N'])
-Places.append([6,'FSGT','Min','Alexandre N'])
-Places.append([6,'FSGT','Min','Alexandre N'])
-Places.append([3,'FSGT','Min','Alexandre N'])
-Places.append([5,'FSGT','Min','Alexandre N'])
-Places.append([4,'FSGT','Min','Alexandre N'])
-
-Places.append([9,'FSGT','Min','Antoine C'])
-Places.append([8,'FSGT','Min','Antoine C'])
-Places.append([2,'FSGT','Min','Antoine C'])
-
-Places.append([2,'FSGT','V4','Jettingen Rene  S'])
-Places.append([4,'FSGT','V4','Jettingen Jerome J'])
-Places.append([2,'FSGT','23','Jettingen Ludo'])
-Places.append([3,'FSGT','23','Jettingen Julien'])
-Places.append([8,'FSGT','23','Jettingen Jerome J '])
-Places.append([4,'FSGT','S4','Jettingen Antoine M '])
-Places.append([9,'FSGT','S4','Jettingen Brieuc H'])
-Places.append([9,'FSGT','Cad','Jettingen Anthony W'])
-
-Places.append([5,'FSGT','V4','Jerome J Rougemont'])
-Places.append([2,'FSGT','Min','Alex N Rougemont'])
-Places.append([3,'FSGT','Min','Antoine C Rougemont'])
-Places.append([7,'FSGT','S4','Antoine M Rougemont'])
-Places.append([4,'FSGT','23','Julien G Rougemont'])
-
-Places.append([6,'FFC','PassD1','FlorianK Monaco'])
-
-Places.append([5,'FSGT','23','FlorianK Plascassier 06'])
-Places.append([1,'FSGT','23','FlorianK Sophia 06'])
-Places.append([4,'FSGT','23','FlorianK Martyrs 06'])
-Places.append([2,'FSGT','23','FlorianK Souvenir morra 06'])
-Places.append([3,'FSGT','23','FlorianK Trappes'])
-
-
-
-Places.append([10,'FFC','23J','FlorianK clm piemont'])
-Places.append([8,'FFC','23J','FlorianK Gerardmer'])
-
-Places.append([10,'FFC','123J','Jerome H Steige'])
-
-Places.append([1,'FFC','PassD1','Antoine M Buhl'])
-Places.append([9,'FFC','PassD1','Jerome J Buhl'])
-
-Places.append([6,'FFC','3J','Julien G Wintershouse'])
-Places.append([8,'FFC','PassD3','Claude S Muguet'])
-Places.append([10,'FFC','PassD3','Gilles E Muguet'])
-
-Places.append([2,'FFC','3J','Julien G Wintershouse'])
-
-Places.append([5,'FFC','23J','Daniel F Sentheim'])
-Places.append([1,'FFC','PassD1','Philippe W Sentheim'])
-Places.append([2,'FFC','3J','Daniel F Merkwiller'])
-
-Places.append([1,'FFC','PassD3','Rene S Schwenheim'])
-Places.append([8,'FFC','PassD3','Claude S Schwenheim'])
-
-Places.append([4,'FFC','PassD1','Kevin E Ornans'])
-Places.append([6,'FFC','PassD1','Kevin E Valdahon'])
-Places.append([4,'FFC','PassD1','Kevin E Pontarlier'])
-Places.append([2,'FFC','PassD1','Kevin E Terre de Chaux'])
-Places.append([2,'FFC','PassD1','Kevin E Morteaux'])
-Places.append([2,'FFC','PassD1','Kevin E Noroy le bourg'])
-Places.append([4,'FFC','PassD1','Kevin E Saint Remy'])
-
-Places.append([1,'FFC','3J','Florian K Montgeron'])
-
-Places.append([8,'FFC','PassD1','Jerome J Charmont'])
-Places.append([10,'FFC','PassD1','Brieuc H Amancey'])
-
-Places.append([6,'FFC','3J','Jerome H crit printemps'])
-
-Places.append([10,'FFC','3J','Julien G TTB 1'])
-Places.append([4,'FFC','3J','Julien G TTB  TTT'])
-Places.append([3,'FFC','3J','Julien G TTB 1'])
-Places.append([2,'FFC','3J','Julien G TTB general'])
-
-Places.append([5,'FFC','23J','Julien G Saone Vingeanne'])
-
-Places.append([9,'FFC','PassD1','Brieuc H ballon '])
-
-Places.append([10,'FFC','23J','Julien B repes'])
-
-Places.append([5,'FFC','23J','Jerome H Ronde Haute Saone 1'])
-Places.append([5,'FFC','23J','Ludo K Xonrupt'])
-
-Places.append([4,'FFC','PassD3','Claude S St Louis'])
-Places.append([6,'FFC','3J','Julien B Marchaux'])
-Places.append([5,'FFC','3J','Florian K Epinay sur Seine'])
-
-
-
-
-
-PlacesTotal = copy.deepcopy(Places)
-
-PlacesTotal.append([2,'FSGT','HC','Eric Heitz VTT'])
-PlacesTotal.append([3,'FSGT','HC','Eric Heitz VTT'])
-PlacesTotal.append([4,'FSGT','HC','Eric Heitz VTT'])
-PlacesTotal.append([5,'FSGT','HC','Eric Heitz VTT'])
-PlacesTotal.append([5,'FSGT','HC','Eric Heitz VTT'])
-PlacesTotal.append([8,'FSGT','HC','Christophe Deis VTT'])
-
-PlacesTotal.append([3,'FSGT','HC','Julien B Grimpee Floridor'])
-PlacesTotal.append([5,'FSGT','HC','Julien B Grimpee Floridor'])
-PlacesTotal.append([1,'FSGT','HC','Julien B Grimpee Salbert'])
-PlacesTotal.append([9,'FFC','HC','Jerome H classement general prestige junior'])
-PlacesTotal.append([4,'FSGT','HC','Gentlemen Lure duo'])
-PlacesTotal.append([2,'FSGT','HC','Alex nanni chrono lure'])
-PlacesTotal.append([1,'FSGT','HC','Alexis Wolff chrono lure'])
-PlacesTotal.append([8,'FSGT','HC','Christophe Deis chrono lure'])
-
-PlacesTotal.append([3,'FFC','HC','Weber Gregory CX Wittenheim'])
-PlacesTotal.append([3,'FFC','HC','Wegner Sonke CX Wittenheim'])
-PlacesTotal.append([8,'FSGT','HC','Jerome H CX Morschwiller'])
-PlacesTotal.append([2,'FSGT','HC','Sonke Wegner CX Heimsbrunn'])
-PlacesTotal.append([7,'FSGT','HC','Alexis Ballester CX Heimsbrunn'])
-
-PlacesTotal.append([4,'FFC','HC','Jerome H CX Saint Bernard'])
-PlacesTotal.append([8,'FFC','HC','Christophe Deis CX Saint Bernard'])
-PlacesTotal.append([1,'FFC','HC','Hugo Hofstetter CX Saint Bernard'])
-
-PlacesTotal.append([3,'FSGT','HC','Sonke W CX Wattwiller'])
-PlacesTotal.append([8,'FSGT','HC','Alexis Wolff CX Wattwiller'])
-PlacesTotal.append([3,'FSGT','HC','Alexis Wolff CX Wattwiller'])
-PlacesTotal.append([6,'FSGT','HC','Christophe D CX Wattwiller'])
-PlacesTotal.append([8,'FSGT','HC','Eric Heitz CX Wattwiller'])
-PlacesTotal.append([10,'FSGT','HC','Alexis B CX Wattwiller'])
-
-PlacesTotal.append([6,'FFC','HC','Jerome H CX Hohrod'])
-
-PlacesTotal.append([9,'FSGT','HC','Alexis W CX Fesches'])
-PlacesTotal.append([5,'FFC','HC','Jerôme H CX Kappelen'])
-
-PlacesTotal.append([9,'FSGT','HC','Eric Heitz CX Staffelfelden'])
+###Coureurs
 
 listeCoureurs = []
 listeCoureurs.append(["Alain L.","AlainL","Pass' D1"])
-listeCoureurs.append(["Alexis B.","AlexisB","S4 (FSGT)"])
-listeCoureurs.append(["Alexis W.","AlexisW","J3 (FSGT)"])
+listeCoureurs.append(["Alexis B.","AlexisB","Pass' D1"])
+listeCoureurs.append(["Alexis W.","AlexisW","Junior"])
 listeCoureurs.append(["Antoine M.","AntoineM","3e caté"])
 listeCoureurs.append(["Arnaud S.","ArnaudS","2e caté"])
 listeCoureurs.append(["Baptiste S.","BaptisteS","Pass' D1"])
@@ -250,6 +34,7 @@ listeCoureurs.append(["Christophe D.","ChristopheD","3e caté"])
 listeCoureurs.append(["Claude K.","ClaudeK","V4 (FSGT)"])
 listeCoureurs.append(["Claude S.","ClaudeS","Pass' D3"])
 listeCoureurs.append(["Daniel F","DanielF","3e caté"])
+listeCoureurs.append(["Emile C.","EmileC","2e caté"])
 listeCoureurs.append(["Eric H.","EricH","3e caté"])
 listeCoureurs.append(["Florian K.","FlorianK","3e caté"])
 listeCoureurs.append(["Florian V.","FlorianV","Pass' D1"])
@@ -259,12 +44,14 @@ listeCoureurs.append(["Guillaume A.","GuillaumeA","Pass' D1"])
 listeCoureurs.append(["Guillaume P.","GuillaumeP","Pass' D1"])
 listeCoureurs.append(["Jérôme H.","JeromeH","Junior"])
 listeCoureurs.append(["Jérôme J.","JeromeJ","Pass' D1"])
+listeCoureurs.append(["Johann V.","JohannV","Junior"])
 listeCoureurs.append(["José L.","JoseL","3e caté"])
 listeCoureurs.append(["Julien B.","JulienB","3e caté"])
 listeCoureurs.append(["Julien G.","JulienG","3e caté"])
 
 listeCoureurs.append(["Kévin E.","KevinE","3e caté"])
 listeCoureurs.append(["Lionel B.","LionelB","Pass' D3"])
+listeCoureurs.append(["Lucas B.","LucasB","2e caté"])
 listeCoureurs.append(["Lucien H.","LucienH","S4 (FSGT)"])
 listeCoureurs.append(["Ludovic K.","LudovicK","2e caté"])
 listeCoureurs.append(["Mathieu N.","MathieuN","3e caté"])
@@ -276,13 +63,14 @@ listeCoureurs.append(["Santo F.","SantoF","3e caté"])
 listeCoureurs.append(["Sébastien S.","SebastienS","Pass' D3"])
 listeCoureurs.append(["Sönke W.","SonkeW","Senior (FSGT)"])
 listeCoureurs.append(["Stéphane M.","StephaneM","V4 (FSGT)"])
+listeCoureurs.append(["Wessel van Hattum.","WesselvH","VTT DH"])
 
 
-listeCoureurs.append(["Johann V.","JohannV","Cadet"])
-listeCoureurs.append(["Alexandre N.","AlexandreN","Minime"])
+listeCoureurs.append(["Alexandre N.","AlexandreN","Cadet"])
 listeCoureurs.append(["Anthony W.","AnthonyW","Cadet"])
 listeCoureurs.append(["Antoine C.","AntoineC","Minime"])
 listeCoureurs.append(["Clément B.","ClementB","Cadet"])
+listeCoureurs.append(["Mathias G.","MathiasG","Minime"])
 '''longueurmax= max([len(coureurs[0]) for coureurs in listeCoureurs])
 for i in listeCoureurs:
     espaceacombler = longueurmax - len(i[0])
@@ -296,12 +84,9 @@ listeCoureurs.append(["Hugo Hofstetter","HugoH","Coureur professionnel - Team Co
 
 coureursDerniereLigne= len(listeCoureurs)%4
 
+### Tableau resultats
+
 list_cates=['FFC123J','FFC23J','FFC3J','FFCPassD1','FFCPassD3','FSGT23','FSGTS4','FSGTV4','FSGTCad','FSGTMin']
-
-
-resultats = collections.OrderedDict()
-for cate in list_cates:
-    resultats[cate]=[0 for i in range(10)]
 
 affichage = {}
 affichage['FFC123J']='FFC : 1-2-3-J'
@@ -318,28 +103,83 @@ affichage['FSGTCad']='FSGT : Cadets'
 affichage['FSGTMin']='FSGT : Minimes'
 affichage['Total']='Total'
 
+
+    ###2017
+
+data = content_xlsx.parse("Resultats2017")
+
+victoires = data[(data.Place == 1)].shape[0]
+top3 = data[(data.Place <= 3)].shape[0]
+top10 = data[(data.Place <= 10)].shape[0]
+
+victoires_fsgt = data[(data.Place == 1) & (data.Fédération == 'FSGT')].shape[0]
+top3_fsgt = data[(data.Place <= 3) & (data.Fédération == 'FSGT')].shape[0]
+top10_fsgt = data[(data.Place <= 10) & (data.Fédération == 'FSGT')].shape[0]
+
+victoires_ffc = data[(data.Place == 1) & (data.Fédération == 'FFC')].shape[0]
+top3_ffc = data[(data.Place <= 3) & (data.Fédération == 'FFC')].shape[0]
+top10_ffc = data[(data.Place <= 10) & (data.Fédération == 'FFC')].shape[0]
+
+bilan = [victoires,victoires_fsgt,victoires_ffc,top3,top3_fsgt,top3_ffc,top10, top10_fsgt, top10_ffc]
+
+
+resultats = collections.OrderedDict()
+for cate in list_cates:
+    resultats[cate]=[0 for i in range(10)]
+
 #print(resultats)
-for perf in Places: #if fsgt or ffc ?
-    resultats[perf[1]+perf[2]][perf[0]-1]+=1   # resultats [FEDEcates] [place-1 à cause de l'indexation] += 1
+for numligne in range(len(data)):
+    if data.iloc[numligne]['Type']== 'Route':
+        cate_nom_ligne = data.iloc[numligne]['Fédération'] + str(data.iloc[numligne]['Catégorie'])
+        resultats[cate_nom_ligne][data.iloc[numligne]['Place']-1]+=1
+    
+#for perf in Places: #if fsgt or ffc ?
+#    resultats[perf[1]+perf[2]][perf[0]-1]+=1   # resultats [FEDEcates] [place-1 à cause de l'indexation] += 1
 
 #print(resultats)
 
 resultats['Total'] = [sum(resultats[cates][place] for cates in list_cates) for place in range(10)]
 
+resultats2017 = copy.deepcopy(resultats)
 
-victoires_fsgt = sum (1 for i in PlacesTotal if i[0]==1 and i[1]=='FSGT')
-top3_fsgt = sum (1 for i in PlacesTotal if i[0]<=3 and i[1]=='FSGT')
-top10_fsgt  = sum (1 for i in PlacesTotal if i[0]<=10 and i[1]=='FSGT')
+   ###2018
 
-victoires_ffc = sum (1 for i in PlacesTotal if i[0]==1 and i[1]=='FFC')
-top3_ffc = sum (1 for i in PlacesTotal if i[0]<=3 and i[1]=='FFC')
-top10_ffc  = sum (1 for i in PlacesTotal if i[0]<=10 and i[1]=='FFC')
+data = content_xlsx.parse("Resultats2018")
 
-victoires= victoires_fsgt + victoires_ffc
-top3=top3_fsgt+top3_ffc
-top10=top10_fsgt+top10_ffc
+victoires = data[(data.Place == 1)].shape[0]
+top3 = data[(data.Place <= 3)].shape[0]
+top10 = data[(data.Place <= 10)].shape[0]
 
-bilan = [victoires,victoires_fsgt,victoires_ffc,top3,top3_fsgt,top3_ffc,top10, top10_fsgt, top10_ffc]
+victoires_fsgt = data[(data.Place == 1) & (data.Fédération == 'FSGT')].shape[0]
+top3_fsgt = data[(data.Place <= 3) & (data.Fédération == 'FSGT')].shape[0]
+top10_fsgt = data[(data.Place <= 10) & (data.Fédération == 'FSGT')].shape[0]
+
+victoires_ffc = data[(data.Place == 1) & (data.Fédération == 'FFC')].shape[0]
+top3_ffc = data[(data.Place <= 3) & (data.Fédération == 'FFC')].shape[0]
+top10_ffc = data[(data.Place <= 10) & (data.Fédération == 'FFC')].shape[0]
+
+bilan2018 = [victoires,victoires_fsgt,victoires_ffc,top3,top3_fsgt,top3_ffc,top10, top10_fsgt, top10_ffc]
+
+
+resultats = collections.OrderedDict()
+for cate in list_cates:
+    resultats[cate]=[0 for i in range(10)]
+
+#print(resultats)
+for numligne in range(len(data)):
+    if data.iloc[numligne]['Type']== 'Route':
+        cate_nom_ligne = data.iloc[numligne]['Fédération'] + str(data.iloc[numligne]['Catégorie'])
+        resultats[cate_nom_ligne][data.iloc[numligne]['Place']-1]+=1
+    
+#for perf in Places: #if fsgt or ffc ?
+#    resultats[perf[1]+perf[2]][perf[0]-1]+=1   # resultats [FEDEcates] [place-1 à cause de l'indexation] += 1
+
+#print(resultats)
+
+resultats['Total'] = [sum(resultats[cates][place] for cates in list_cates) for place in range(10)]
+
+resultats2018 = copy.deepcopy(resultats)
+
 
 index=True
 template = env.get_template('index.jj') #nomdutemplate
@@ -351,7 +191,7 @@ ofh.close()
 index=False
 template = env.get_template('performances.jj') #nomdutemplate
 ofh = codecs.open("performances.html","w", encoding="utf-8")
-rt = template.render(resultats=resultats, bilan=bilan,affichage=affichage, index=index)
+rt = template.render(resultats2018=resultats2018, resultats2017 = resultats2017, bilan=bilan,affichage=affichage, index=index)
 ofh.write(rt)
 ofh.close()
 
@@ -402,4 +242,3 @@ ofh = codecs.open("organisations.html","w", encoding="utf-8")
 rt = template.render(bilan=bilan, index=index)
 ofh.write(rt)
 ofh.close()
-
